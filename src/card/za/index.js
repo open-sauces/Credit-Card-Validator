826f87f587d4684d8434cb49a24112094452d5b2
martenterblanche@gmail.com <martenterblanche@gmail.com>
2017-02-12 17:00:04 +0200

    Updates on tests

--- src/card/za/index.js
+++ src/card/za/index.js
@@ -1,64 +1,80 @@
-export const test = () => {
-
-  //Tests
-    let lastCharacterOnCard = 5;
-    let testVisaComplete    = 4556737586899855;
-    let testVisaWithoutLast = 455673758689985;
-    let visaInReverse = 589986857376554;
-    let visaEvenOddIndexNumber = [ 8965365, 59887754 ];
-    let workedOutMaths = 19775518;
-    let allNumbersAdded = 85;
-    let moduloTenCalc = 5;
+// VISA:
+// 4556197518505296 - true
+// 4539991607481548 - false
+// 4929786148583630 - true
 
 
+//Variables
+  const VISA = 4556737586899855;
+  const MASTERCARD = 5483860808597696;
+  const AMERICANEXPRESS = 349973529889097;
+  const DISCOVER = 6011629506225960;
+  const JCB = 3528216329036270;
+  const VISAELECTRON = 4508405849060314;
+  // const MAESTRO = 0604215418083568;
 
-    describe('getLast()', () => {
-      it('Get last character of string and returns a number', () => {
-        expect(getLast(testVisaComplete)).toEqual(5);
-      });
-    });
+  const TESTCARD = 4556737586899855;
 
-    describe('removeLast()', () => {
-      it('Remove last character of string and returns a number', () => {
-        expect(removeLast(testVisaComplete)).toEqual(455673758689985);
-      });
-    });
+  function validateCard(cardToTest){
+    console.log('Card being tested', cardToTest);
+    const lastNumber = getLast(cardToTest);
 
-    describe('reverseOrder()', () => {
-      it('Reverse the order of the passed in string', () => {
-        expect(reverseOrder(testVisaWithoutLast)).toEqual(589986857376554);
-      });
-    });
+    return comparison(
+            moduloTen(
+              addAll(arrayItemsMultiply(getOddEvenIndex(reverseOrder(removeLast(cardToTest)))[1]), getOddEvenIndex(reverseOrder(removeLast(cardToTest)))[0])
+            ), lastNumber
+          );
+  }
+
+  console.log('Was your card valid? - ', validateCard(4539991607481548));
+
+//Calculations
+
+  let lastNumberOnCard = getLast(4539991607481548);
+  let cardWithoutLastNumber = removeLast(4539991607481548);
+  let cardInReverseOrder = reverseOrder(4539991607481548);
+  // let getOdd = getOddEvenIndex(cardInReverseOrder);
 
 
-    describe('getOddEvenIndex()', () => {
-      it('gets the even index values in given string', () => {
-        expect(getOddEvenIndex(visaInReverse)).toEqual([ 8965365, 59887754 ]);
-      });
-    });
+//Functions
+  function getLast(a){return Number(String(a).slice(-1))};
+  function removeLast(a){return Number(String(a).slice(0, -1))};
+  function reverseOrder(a) {return Number(Array.from(String(a)).reverse().join(''));};
+  function getOddEvenIndex(value) {
+    let splitNumber =  String(value).split('');
+    var evenPositions = [];
+    var oddPositions = [];
+    var number = splitNumber;
+    for(var i = 0; i < number.length; i++)
+    (i % 2 == 0 ? oddPositions : evenPositions).push(number[i]);
+        return [Number(evenPositions.join('')), Number(oddPositions.join(''))];
+  }
 
-    describe('arrayItemsMultiply()', () => {
-      it('Multiply the digits in odd positions by 2 and subtract 9 to all any result higher than 9', () => {
-        expect(arrayItemsMultiply(visaEvenOddIndexNumber[1])).toEqual(19775518);
-      });
-    });
+  function arrayItemsMultiply(value){
+    let splitNumber =  String(value).split('');
+    let calculated = splitNumber.map(function(item){
+      item = Number(item) * 2;
+      if(Number(item) >= 9){
+        item = item - 9;
+      }
+      return item;
+    })
+    return Number(calculated.join(''));
+  }
 
-    describe('addAll()', () => {
-      it('Add all numbers', () => {
-        expect(addAll(workedOutMaths, visaEvenOddIndexNumber[0])).toEqual(85);
-      });
-    });
+  function addAll(a, b){
+    let splitNumber =  String(a).split('');
+    let numberA =  String(a).split('');
+    let numberB =  String(b).split('');
+    Array.prototype.push.apply(numberA, numberB);
+    // var total = sports.push('football', 'swimming');
+    let total = numberA.reduce((a, b) => a + Number(b), 0);
+    return Number(total);
+  };
 
-    describe('moduloTen()', () => {
-      it('Module is used to get the remainder after integer division', () => {
-        expect(moduloTen(allNumbersAdded)).toEqual(5);
-      });
-    });
-
-    describe('comparison()', () => {
-      it('Strictly compares 2 given values to eachother', () => {
-        expect(comparison(moduloTenCalc, lastCharacterOnCard)).toEqual(true);
-      });
-    });
-
-};
+  function moduloTen(value){
+    return value % 10;
+  }
+  function comparison(a, b){
+    return a === b ? true : false;
+  }
--- src/card/za/index.test.js
+++ src/card/za/index.test.js
@@ -1,82 +1,126 @@
-// VISA:
-// 4556197518505296 - true
-// 4539991607481548 - false
-// 4929786148583630 - true
+// //Tests
+//   let lastCharacterOnCard = 5;
+//   let testVisaComplete    = 589986857376554;
+//   let testVisaWithoutLast = 58998685737655;
+//   let visaInReverse = 589986857376554;
+//   let visaEvenOddIndexNumber = [ 8965365, 59887754 ];
+//   let workedOutMaths = 19775518;
+//   let allNumbersAdded = 85;
+//   let moduloTenCalc = 5;
+//
+//
+//
+//   describe('getLast()', () => {
+//     it('Get last character of string and returns a number', () => {
+//       expect(getLast(testVisaComplete)).toEqual(5);
+//     });
+//   });
+//
+//   describe('removeLast()', () => {
+//     it('Remove last character of string and returns a number', () => {
+//       expect(removeLast(testVisaComplete)).toEqual(58998685737655);
+//     });
+//   });
+//
+//   describe('reverseOrder()', () => {
+//     it('Reverse the order of the passed in string', () => {
+//       expect(reverseOrder(testVisaWithoutLast)).toEqual(589986857376554);
+//     });
+//   });
+//
+//
+//   describe('getOddEvenIndex()', () => {
+//     it('gets the even index values in given string', () => {
+//       expect(getOddEvenIndex(visaInReverse)).toEqual([ 8965365, 59887754 ]);
+//     });
+//   });
+//
+//   describe('arrayItemsMultiply()', () => {
+//     it('Multiply the digits in odd positions by 2 and subtract 9 to all any result higher than 9', () => {
+//       expect(arrayItemsMultiply(visaEvenOddIndexNumber[1])).toEqual(19775518);
+//     });
+//   });
+//
+//   describe('addAll()', () => {
+//     it('Add all numbers', () => {
+//       expect(addAll(workedOutMaths, visaEvenOddIndexNumber[0])).toEqual(85);
+//     });
+//   });
+//
+//   describe('moduloTen()', () => {
+//     it('Module is used to get the remainder after integer division', () => {
+//       expect(moduloTen(allNumbersAdded)).toEqual(5);
+//     });
+//   });
+//
+//   describe('comparison()', () => {
+//     it('Strictly compares 2 given values to eachother', () => {
+//       expect(comparison(moduloTenCalc, lastCharacterOnCard)).toEqual(true);
+//     });
+//   });
 
 
 
 
-//Variables
-  const VISA = 4556737586899855;
-  const MASTERCARD = 5483860808597696;
-  const AMERICANEXPRESS = 349973529889097;
-  const DISCOVER = 6011629506225960;
-  const JCB = 3528216329036270;
-  const VISAELECTRON = 4508405849060314;
-  // const MAESTRO = 0604215418083568;
-
-  const TESTCARD = 4556737586899855;
-
-  function validateCard(cardToTest){
-    console.log('Card being tested', cardToTest);
-    const lastNumber = getLast(cardToTest);
-
-    return comparison(
-            moduloTen(
-              addAll(arrayItemsMultiply(getOddEvenIndex(reverseOrder(removeLast(cardToTest)))[1]), getOddEvenIndex(reverseOrder(removeLast(cardToTest)))[0])
-            ), lastNumber
-          );
-  }
-
-  console.log('Was your card valid? - ', validateCard(4539991607481548));
-
-//Calculations
-
-  let lastNumberOnCard = getLast(4539991607481548);
-  let cardWithoutLastNumber = removeLast(4539991607481548);
-  let cardInReverseOrder = reverseOrder(4539991607481548);
-  // let getOdd = getOddEvenIndex(cardInReverseOrder);
 
 
-//Functions
-  function getLast(a){return Number(String(a).slice(-1))};
-  function removeLast(a){return Number(String(a).slice(0, -1))};
-  function reverseOrder(a) {return Number(Array.from(String(a)).reverse().join(''));};
-  function getOddEvenIndex(value) {
-    let splitNumber =  String(value).split('');
-    var oddOnes = [],
-     evenOnes = [];
-    var number = splitNumber;
-    for(var i=0; i<number.length; i++)
-    (i % 2 == 0 ? evenOnes : oddOnes).push(number[i]);
-        return [Number(oddOnes.join('')), Number(evenOnes.join(''))];
-  }
 
-  function arrayItemsMultiply(value){
-    let splitNumber =  String(value).split('');
-    let calculated = splitNumber.map(function(item){
-      item = Number(item) * 2;
-      if(Number(item) >= 9){
-        item = item - 9;
-      }
-      return item;
-    })
-    return Number(calculated.join(''));
-  }
 
-  function addAll(a, b){
-    let splitNumber =  String(a).split('');
-    let numberA =  String(a).split('');
-    let numberB =  String(b).split('');
-    Array.prototype.push.apply(numberA, numberB);
-    // var total = sports.push('football', 'swimming');
-    let total = numberA.reduce((a, b) => a + Number(b), 0);
-    return Number(total);
-  };
+  //Tests
+      const cardGivesFalse = 4539991607481548;
+      let visaInReverse = 8451847061999354;
 
-  function moduloTen(value){
-    return value % 10;
-  }
-  function comparison(a, b){
-    return a === b ? true : false;
-  }
+    //   let visaEvenOddIndexNumber = [ 8965365, 59887754 ];
+    //   let workedOutMaths = 19775518;
+    //   let allNumbersAdded = 85;
+    //   let moduloTenCalc = 5;
+
+
+
+    describe('getLast()', () => {
+      it('Get last character of string and returns a number', () => {
+        expect(getLast(cardGivesFalse)).toEqual(8);
+      });
+    });
+
+    describe('removeLast()', () => {
+      it('Remove last number', () => {
+        expect(removeLast(cardGivesFalse)).toEqual(453999160748154);
+      });
+    });
+
+    describe('reverseOrder()', () => {
+      it('Reverse the order of number', () => {
+        expect(reverseOrder(removeLast(cardGivesFalse))).toEqual(451847061999354);
+      });
+    });
+
+    describe('getOddEvenIndex()', () => {
+      it('gets the [ evenPositions, oddPositions ] index values and puts them in array', () => {
+        expect(getOddEvenIndex(reverseOrder(removeLast(cardGivesFalse)))).toEqual([ 5876995, 41401934 ]);
+      });
+    });
+
+    describe('arrayItemsMultiply()', () => {
+      it('Multiply the digits in odd positions by 2 and subtract 9 to all/any result higher than 9', () => {
+        expect(arrayItemsMultiply(getOddEvenIndex(reverseOrder(removeLast(cardGivesFalse)))[1])).toEqual(82802968);
+      });
+    });
+
+    describe('addAll()', () => {
+      it('Add all numbers', () => {
+        expect(addAll(arrayItemsMultiply(getOddEvenIndex(reverseOrder(removeLast(cardGivesFalse)))[1]), getOddEvenIndex(reverseOrder(removeLast(cardGivesFalse)))[0])).toEqual(92);
+      });
+    });
+
+    describe('moduloTen()', () => {
+      it('Module is used to get the remainder after integer division', () => {
+        expect(moduloTen(addAll(arrayItemsMultiply(getOddEvenIndex(reverseOrder(removeLast(cardGivesFalse)))[1]), getOddEvenIndex(reverseOrder(removeLast(cardGivesFalse)))[0]))).toEqual(getLast(cardGivesFalse));
+      });
+    });
+    //
+  //   describe('comparison()', () => {
+  //     it('Strictly compares 2 given values to eachother', () => {
+  //       expect(comparison(moduloTenCalc, lastCharacterOnCard)).toEqual(true);
+  //     });
+  //   });
